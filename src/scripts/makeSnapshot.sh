f65dfb60baf0496bb83cc14b402f6b646cde26eb
eivaskevicius <e.ivaskevicius@iplabs.de>
2018-03-01 17:39:50 +0100

    problem

--- src/scripts/makeSnapshot.sh
+++ src/scripts/makeSnapshot.sh
@@ -5,7 +5,8 @@ set -e -u -x
 git clone resource-sbalpi-dev snapshot
 cp -r node_modules snapshot
 cd snapshot
+ls -la
 git config --global user.email "e.ivaskevicius@iplabs.de"
 git config --global user.name "eivaskevicius"
-git add -A
-git commit -m "add the functioning modules"
+# git add -A
+# git commit -m "add the functioning modules"
