7ac8a3e7032716a6260cc0df11535c264e606c81
eivaskevicius <e.ivaskevicius@iplabs.de>
2018-03-02 12:25:54 +0100

    move snapshot

--- src/scripts/moveSnapshot.sh
+++ src/scripts/moveSnapshot.sh
@@ -4,9 +4,10 @@ set -e -u -x
 
 git config --global user.email "e.ivaskevicius@iplabs.de"
 git config --global user.name "eivaskevicius"
-git clone resource-sbalpi-dev snapshot
-cp -r node_modules snapshot
-cd snapshot
+git clone resource-sbalpi-snap snapshot-node
+cp snapshot/sbalpi* snapshot-node
+
+cd snapshot-node
 
 npm remove jest
 rm -r src/__test__
@@ -15,7 +16,7 @@ npm pack
 
 ls -la
 
-# git add -A
-# git status
-# git commit -m "add snapshot"
-# git pull
+git add -A
+git status
+git commit -m "add snapshot"
+git pull
