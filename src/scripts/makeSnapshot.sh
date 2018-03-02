6f942c1f1bc27878b19bad0983dd92503f3273a9
eivaskevicius <e.ivaskevicius@iplabs.de>
2018-03-02 11:14:25 +0100

    remove tests

--- src/scripts/makeSnapshot.sh
+++ src/scripts/makeSnapshot.sh
@@ -5,9 +5,11 @@ set -e -u -x
 git clone resource-sbalpi-dev snapshot
 cp -r node_modules snapshot
 cd snapshot
+npm remove jest
+rm -r src/__test__
 npm pack
 git config --global user.email "e.ivaskevicius@iplabs.de"
 git config --global user.name "eivaskevicius"
 git add -A
 git status
-git commit -m "add the functioning modules"
+git commit -m "add snapshot"
