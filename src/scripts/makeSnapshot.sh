9baef4f0fa193b27933e2d41bc35fc79d57115a9
eivaskevicius <e.ivaskevicius@iplabs.de>
2018-03-02 11:23:31 +0100

    check the zipped file

--- src/scripts/makeSnapshot.sh
+++ src/scripts/makeSnapshot.sh
@@ -2,14 +2,19 @@
 
 set -e -u -x
 
+git config --global user.email "e.ivaskevicius@iplabs.de"
+git config --global user.name "eivaskevicius"
 git clone resource-sbalpi-dev snapshot
 cp -r node_modules snapshot
 cd snapshot
+
 npm remove jest
 rm -r src/__test__
+
 npm pack
-git config --global user.email "e.ivaskevicius@iplabs.de"
-git config --global user.name "eivaskevicius"
+
+ls -la
+
 git add -A
 git status
 git commit -m "add snapshot"
