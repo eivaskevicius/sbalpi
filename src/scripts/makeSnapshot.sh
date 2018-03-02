91d1327fda1fb0b0e58a0cf63678f9c98e67ca8a
eivaskevicius <e.ivaskevicius@iplabs.de>
2018-03-02 11:04:53 +0100

    add npm pack

--- src/scripts/makeSnapshot.sh
+++ src/scripts/makeSnapshot.sh
@@ -5,7 +5,7 @@ set -e -u -x
 git clone resource-sbalpi-dev snapshot
 cp -r node_modules snapshot
 cd snapshot
-ls -la
+npm pack
 git config --global user.email "e.ivaskevicius@iplabs.de"
 git config --global user.name "eivaskevicius"
 git add -A
